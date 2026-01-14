31222f83eb4223d45746264bd6e11a091fb81bba
Nikhil Kumar <sahilkumar9045745602@gmail.com>
2026-01-14 22:06:11 +0530

    button fetchvideo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
-import { fetchPhotos } from './api/mediaApi'
+import { fetchPhotos, fetchVideos } from './api/mediaApi'
 
 const App = () => {
 
  
   return (
-    <div className='h-screen w-full bg-zinc-900 text-white'>
+    <div className='h-screen w-full  bg-zinc-900 text-white'>
       <button onClick={async ()=>{
        const data = await fetchPhotos('cat')
 
        console.log(data.results)
-      }} className='bg-red-600 border rounded px-6'>Get Photos</button>
+      }} className='bg-red-600 border  mr-6 rounded px-6'>Get Photos</button>
+      
+      <button onClick={async ()=>{
+       const data = await fetchVideos('dog')
+
+       console.log(data.results)
+      }} className='bg-yellow-300 border rounded px-6'>Get Video</button>
     </div>
   )
 }
