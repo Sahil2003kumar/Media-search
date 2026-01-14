28d3860d0fd5d6abb6087c4e15976489e68017c6
Nikhil Kumar <sahilkumar9045745602@gmail.com>
2026-01-14 21:43:51 +0530

    complete fetchvideo function

--- src/api/mediaApi.js
+++ src/api/mediaApi.js
@@ -17,10 +17,10 @@ const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY
 }
 
 //this function used for to fetch the video
-export async function fetchVideos(query, page=1, per_page=15){
-    const res = await axios.get('https://api.unsplash.com/search/photos',{
-    params:{query,page,per_page},
-    headers:{Authorization:`Client-ID ${UNSPLASH_KEY}`}
+export async function fetchVideos(query,  per_page=15){
+    const res = await axios.get('https://api.pexels.com/videos/search',{
+    params:{query,per_page},
+    headers:{Authorization:PEXELS_KEY}
 
    })
    return res.data
