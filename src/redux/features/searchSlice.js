3634718f4fa14b851ac84e87400607058f3ee669
Nikhil Kumar <sahilkumar9045745602@gmail.com>
2026-01-14 23:04:46 +0530

    searchSlice wroking

--- src/redux/features/searchSlice.js
+++ src/redux/features/searchSlice.js
@@ -2,5 +2,29 @@ import { createSlice } from "@reduxjs/toolkit";
 
 
 const searchSlice = createSlice({
-    
+    name:"search",
+    initialState:{
+        query:'',
+        activeTab:'photos',
+        results:[],
+        loading:false,
+        error:null
+    },
+    reducers:{
+        setQuery(state, action){
+
+        },
+        setAciveTabs(state,action){
+
+        },
+        setoading(state,action){
+
+        },
+        setResults(state,action){
+
+        },
+        setError(state,action){
+            
+        }
+    }
 })
