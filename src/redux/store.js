9995f5163fdd26ffa6e5d9185a94abad2c37294e
Nikhil Kumar <sahilkumar9045745602@gmail.com>
2026-01-15 12:25:37 +0530

    store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import searchReducer from './features/searchSlice'
 // making the store 
 export const store = configureStore({
-    reducer:{}
+    reducer:{
+        search:searchReducer,
+    }
 })
