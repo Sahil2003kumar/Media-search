becdfbd0d5e16d5a9b76afce03ae92530917a9d9
Nikhil Kumar <sahilkumar9045745602@gmail.com>
2026-01-15 12:31:55 +0530

    SearchBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import React from 'react'
-import { fetchPhotos, fetchVideos } from './api/mediaApi'
+import SearchBar from './components/SearchBar'
+
 
 const App = () => {
 
  
   return (
     <div className='h-screen w-full  bg-zinc-900 text-white'>
-      <button onClick={async ()=>{
-       const data = await fetchPhotos('cat')
-
-       console.log(data.results)
-      }} className='bg-red-600 border  mr-6 rounded px-6'>Get Photos</button>
-      
-      <button onClick={async ()=>{
-       const data = await fetchVideos('dog')
-
-       console.log(data.videos)
-      }} className='bg-yellow-300 border rounded px-6'>Get Video</button>
+     <SearchBar/>
     </div>
   )
 }
