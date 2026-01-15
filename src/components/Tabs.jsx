73fbf0e3965899de592b01a4eb61f956dfda662e
Nikhil Kumar <sahilkumar9045745602@gmail.com>
2026-01-15 17:00:26 +0530

    tabs completed

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,19 +1,30 @@
 //making tabs
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { setActiveTabs } from '../redux/features/searchSlice'
 
 const Tabs = () => {
-    const tabs = ['photos','video', 'GIF']
-  return (
-    <div className='flex gap-5 p-10'>
-        {tabs.map(function(elem,idx){
-            return <button 
-            className='bg-gray-600 cursor-pointer active:scale-95 px-5 py-2 rounded uppercase hover:bg-green-400'
-            key={idx}>
-                {elem}
-                </button>
-        })}
-    </div>
-  )
+    const tabs = ['photos', 'video', 'GIF']
+    const dispatch = useDispatch()
+
+    const activeTab = useSelector((state)=>state.search.activeTab)
+    return (
+        <div className='flex gap-5 p-10'>
+            {tabs.map(function (elem, idx) {
+                return (
+                    <button
+                        className={`${(activeTab == elem ? 'bg-green-400' : 'bg-zinc-600')} transition cursor-pointer active:scale-95 px-5 py-2 rounded uppercase `}
+                        key={idx}
+                        onClick={() => {
+                            dispatch(setActiveTabs(elem))
+                        }}
+                    >
+                        {elem}
+                    </button>
+                )
+            })}
+        </div>
+    )
 }
 
 export default Tabs
