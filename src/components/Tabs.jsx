bcb0ffc31372b9b2e3d22c0253b7ccd1495717d3
Nikhil Kumar <sahilkumar9045745602@gmail.com>
2026-01-15 13:24:12 +0530

    tab ongoing

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,1 +1,19 @@
 //making tabs
+import React from 'react'
+
+const Tabs = () => {
+    const tabs = ['photos','video', 'GIF']
+  return (
+    <div className='flex gap-5 p-10'>
+        {tabs.map(function(elem,idx){
+            return <button 
+            className='bg-gray-600 cursor-pointer active:scale-95 px-5 py-2 rounded uppercase hover:bg-green-400'
+            key={idx}>
+                {elem}
+                </button>
+        })}
+    </div>
+  )
+}
+
+export default Tabs
