1d60bf9679d7c35f3dfbaaa0abf66dda93c4f62d
Nikhil Kumar <sahilkumar9045745602@gmail.com>
2026-01-19 18:16:58 +0530

    dd

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const ResultCard = (props) => {
+const ResultCard = ({item}) => {
   return (
-    <div className='w-60 h-80 bg-white rounded'>
-        <h1 className='text-black'>{props.item.title}</h1>
+    <div className='w-[18vw] relative h-70  rounded'>
+      <a target='_blank'className='h-full' href={item.url}>
+         {item.type =='photo'?<img className='h-full w-full rounded-2xl object-cover object-center' src={item.src} alt="" />:''}
+        {item.type =='video'?<video className='h-full w-full rounded-2xl object-cover object-center' autoPlay loop muted src={item.src}></video>:''}
+       </a>
+        <div id='bottom' className=' flex justify-between w-full px-1 py-10 items-center absolute bottom-0 text-white'>
+            <h2 className='text-sm font-semibold capitalize'>{item.title}</h2>
+            <button className='bg-lime-500 cursor-pointer text-white rounded px-3 py-1 font-medium'>Save</button>
+        </div>
     </div>
   )
 }
