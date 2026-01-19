d682f6ada96ca01dccf3e357fd47d275d578599e
Nikhil Kumar <sahilkumar9045745602@gmail.com>
2026-01-19 17:13:06 +0530

    ..

--- src/components/ResultGrid.jsx
+++ src/components/ResultGrid.jsx
@@ -1,48 +1,67 @@
 //startind result grid
 import React, { useEffect } from 'react'
 import { fetchPhotos, fetchVideos } from '../api/mediaApi'
-import { setQuery, setLoading, setError, setResults } from '../redux/features/searchSlice'
+import { setLoading, setError, setResults } from '../redux/features/searchSlice'
 import { useDispatch, useSelector } from 'react-redux'
+import ResultCard from './ResultCard'
 
 const ResultGrid = () => {
 
     const { query, activeTab, results, loading, error } = useSelector((store) => store.search)
 
-    const getData = async () => {
+    const dispatch = useDispatch()
 
-        let data
 
-        if (activeTab == 'photos') {
-            let response = await fetchPhotos(query)
-            //doing normalization
-            data = response.results.map((item)=>({
-                id:item.id,
-                type:'photo',
-                title:item.alt_description,
-                thumbnail:item.urls.small,
-                src:item.urls.full
-            }))
-        }
-        if (activeTab == 'video') {
-            let response = await fetchVideos(query)
-            data = response.videos.map((item)=>({
-                id:item.id,
-                type:'video',
-                title:item.user.name || 'video',
-                thumbnail:item.image,
-                src:item.video_files[0].link
-            }))
-        }
-        console.log(data)
-
-    }
 
     useEffect(function () {
+        if (!query) return
+        const getData = async () => {
+
+            try {
+                dispatch(setLoading())
+                let data = []
+
+                if (activeTab == 'photos') {
+                    let response = await fetchPhotos(query)
+                    //doing normalization
+                    data = response.results.map((item) => ({
+                        id: item.id,
+                        type: 'photo',
+                        title: item.alt_description,
+                        thumbnail: item.urls.small,
+                        src: item.urls.full
+                    }))
+                }
+                if (activeTab == 'video') {
+                    let response = await fetchVideos(query)
+                    data = response.videos.map((item) => ({
+                        id: item.id,
+                        type: 'video',
+                        title: item.user.name || 'video',
+                        thumbnail: item.image,
+                        src: item.video_files[0].link
+                    }))
+                }
+
+                dispatch(setResults(data))
+            } catch (error) {
+                dispatch(setError(error.message))
+            }
+        }
+
         getData()
     }, [query, activeTab])
-    return (
-        <div>
 
+    if (error) return <h1>Error</h1>
+    if (loading) return <h1>Loading...</h1>
+    return (
+        <div className='flex h-[85%] justify-center flex-wrap gap-5 overflow-auto px-10 '> 
+
+            {results.map((item, idx) => {
+                return <div key={idx}>
+                    <ResultCard item={item}/>
+                </div>
+            })}
         </div>
     )
 }
