a2d282bb6735b294e9c88f29d01e347105c30d0e
Nikhil Kumar <sahilkumar9045745602@gmail.com>
2026-01-21 12:27:14 +0530

    colllection cardjsx

---
+++ src/components/CollectionCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const CollectionCard = ({item}) => {
+  return (
+   <div className='w-[18vw] relative h-70  rounded'>
+      <a target='_blank'className='h-full' href={item.url}>
+         {item.type =='photo'?<img className='h-full w-full rounded-2xl object-cover object-center' src={item.src} alt="" />:''}
+        {item.type =='video'?<video className='h-full w-full rounded-2xl object-cover object-center' autoPlay loop muted src={item.src}></video>:''}
+       </a>
+        <div id='bottom' className=' flex justify-between w-full px-1 py-10 items-center absolute bottom-0 text-white'>
+            <h2 className='text-sm font-semibold capitalize'>{item.title}</h2>
+            <button 
+            onClick={()=>{
+             console.log("removed")
+            }}
+            className='bg-lime-500 cursor-pointer active:scale-95 text-white rounded px-3 py-1 font-medium'>Remove</button>
+        </div>
+    </div>
+  )
+}
+
+export default CollectionCard
