830ae9b575ef26de7fbe78e03a82b29524fb91a8
Nikhil Kumar <sahilkumar9045745602@gmail.com>
2026-01-21 13:18:30 +0530

    done

--- src/pages/CollectionPage.jsx
+++ src/pages/CollectionPage.jsx
@@ -1,35 +1,38 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import CollectionCard from '../components/collectionCard'
-import {  clearCollection } from '../redux/features/collectionSlice'
+import { clearCollection } from '../redux/features/collectionSlice'
 
 const CollectionPage = () => {
 
   const dispatch = useDispatch()
   const collection = useSelector(state => state.collection.items)
-  const clearAllcollection = ()=>{
-dispatch(clearCollection())
+  const clearAllcollection = () => {
+    dispatch(clearCollection())
   }
   return (
-   <div className='overflow-auto px-10 p-20'>
-    <div className='flex justify-between'>
-      <h2 className='text-3xl font-medium '>Your Collection</h2>
-      <button
-      onClick={()=>{
-        
-      clearAllcollection()
-      }}
-      className='bg-red-600 px-6  text-base font-medium rounded  active:scale-90'>Clear All Collection</button>
+    <div className='overflow-auto px-10 p-20'>
 
-    </div>
-     <div className='flex h-[85%] w-full justify-center flex-wrap gap-5  mt-5 '>
-        {collection.map((item, idx)=>{
+      {collection.length > 0 ? <div className='flex justify-between'>
+        <h2 className='text-3xl font-medium '>
+          Your Collection
+        </h2>
+        <button
+          onClick={() => {
+            clearAllcollection()
+          }}
+          className='bg-red-600 px-6  text-base font-medium rounded  cursor-pointer active:scale-90'>Clear All Collection</button>
+      </div> : <h2 className='text-3xl font-medium  text-center text-zinc-500'>Your Collection is Empty
+        </h2>}
+
+      <div className='flex h-[85%] w-full justify-center flex-wrap gap-5  mt-5 '>
+        {collection.map((item, idx) => {
           return <div key={idx}>
             <CollectionCard item={item} />
-             </div>
+          </div>
         })}
+      </div>
     </div>
-   </div>
   )
 }
 
