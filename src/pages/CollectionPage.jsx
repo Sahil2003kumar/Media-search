302111117554f098091f138e5ea1ec3aa6f6f283
Nikhil Kumar <sahilkumar9045745602@gmail.com>
2026-01-21 12:26:59 +0530

    dd

--- src/pages/CollectionPage.jsx
+++ src/pages/CollectionPage.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import CollectionCard from '../components/collectionCard'
 
 const CollectionPage = () => {
+
+  const collection = useSelector(state => state.collection.items)
   return (
-    <div>
-        
+    <div className='flex h-[85%] w-full justify-center flex-wrap gap-5 overflow-auto px-10 p-20 '>
+        {collection.map((item, idx)=>{
+          return <div key={idx}>
+            <CollectionCard item={item} />
+             </div>
+        })}
     </div>
   )
 }
