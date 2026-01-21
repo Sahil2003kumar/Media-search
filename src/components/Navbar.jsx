9a0f1dcd0666f656fa7cd39d4e95dc9c1512e699
Nikhil Kumar <sahilkumar9045745602@gmail.com>
2026-01-21 13:05:10 +0530

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
         
          <div className='flex justify-between items-center py-4 px-10 bg-slate-400'>
 
-                <h2 className='font-medium text-2xl'>Media Search</h2>
+                <Link to='/' className='font-medium text-2xl'>Media Search</Link>
                 <div className='flex gap-5 items-center'>
                     <Link className='text-base font-medium active:scale-90 bg-emerald-500 rounded-xl px-4 py-2' to='/'>Search</Link>
                     <Link className='text-base font-medium active:scale-90 bg-emerald-500 rounded-xl px-4 py-2' to='/collection'>Collection</Link>
