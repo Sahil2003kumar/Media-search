d47acbf9e613b4aa6b3f2d0d2302f399ec837f68
Nikhil Kumar <sahilkumar9045745602@gmail.com>
2026-01-21 13:05:01 +0530

    chnges done in collection card

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import HomePage from './pages/HomePage'
 import CollectionPage from './pages/CollectionPage'
 
 import { ToastContainer } from 'react-toastify';
+import Navbar from './components/Navbar';
 
 const App = () => {
 
  
   return (
     <div className='min-h-screen w-full  bg-zinc-900 text-white'>
-    
+    <Navbar/>
     <Routes>
       <Route path='/' element={<HomePage/>} />
       <Route path='/collection' element={<CollectionPage/>} />
--- src/components/CollectionCard.jsx
+++ src/components/CollectionCard.jsx
@@ -1,31 +1,35 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { removeCollection, removeToast } from '../redux/features/collectionSlice'
+import Navbar from './Navbar'
 
-const CollectionCard = ({item}) => {
+const CollectionCard = ({ item }) => {
 
     const dispatch = useDispatch()
 
-    const removeFromCollection = (item)=>{
-dispatch(removeCollection(item.id))
-dispatch(removeToast())
+    const removeFromCollection = (item) => {
+        dispatch(removeCollection(item.id))
+        dispatch(removeToast())
     }
-  return (
-   <div className='w-[18vw] relative h-70  rounded'>
-      <a target='_blank'className='h-full' href={item.url}>
-         {item.type =='photo'?<img className='h-full w-full rounded-2xl object-cover object-center' src={item.src} alt="" />:''}
-        {item.type =='video'?<video className='h-full w-full rounded-2xl object-cover object-center' autoPlay loop muted src={item.src}></video>:''}
-       </a>
-        <div id='bottom' className=' flex justify-between w-full px-1 py-10 items-center absolute bottom-0 text-white'>
-            <h2 className='text-sm font-semibold capitalize'>{item.title}</h2>
-            <button 
-            onClick={()=>{
-            removeFromCollection(item)
-            }}
-            className='bg-lime-500 cursor-pointer active:scale-95 text-white rounded px-3 py-1 font-medium'>Remove</button>
-        </div>
-    </div>
-  )
-}
+    return (
+        
+            <div className='w-[18vw] relative h-70  rounded'>
 
+
+                <a target='_blank' className='h-full' href={item.url}>
+                    {item.type == 'photo' ? <img className='h-full w-full rounded-2xl object-cover object-center' src={item.src} alt="" /> : ''}
+                    {item.type == 'video' ? <video className='h-full w-full rounded-2xl object-cover object-center' autoPlay loop muted src={item.src}></video> : ''}
+                </a>
+                <div id='bottom' className=' flex justify-between w-full px-1 py-10 items-center absolute bottom-0 text-white'>
+                    <h2 className='text-sm font-semibold capitalize'>{item.title}</h2>
+                    <button
+                        onClick={() => {
+                            removeFromCollection(item)
+                        }}
+                        className='bg-lime-500 cursor-pointer active:scale-95 text-white rounded px-3 py-1 font-medium'>Remove</button>
+                </div>
+            </div>
+      
+            )
+}
 export default CollectionCard
